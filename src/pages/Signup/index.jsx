6d731710028fe82961602b29ffa65729c96c060b
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-01-29 10:07:18 +0000

    Signup UI done

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -11,9 +11,23 @@ import { Outlet, Link } from 'react-router-dom';
 function Signup(){
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [password2, setPassword2] = useState("");
+    const [signupErrorMsg, setSignupErrorMsg] = useState("");
 
     const handleSignUp = async (e) => {
         e.preventDefault();
+
+        if(username.trim() === "" || password.trim() === "" || password2.trim() === ""){
+            setSignupErrorMsg("Fill all fields");
+        }else if(password !== password2){
+            setSignupErrorMsg("Passwords does not match");
+        }else{
+            //w
+        }
+    }
+
+    function resetErrorMsg(){
+        setSignupErrorMsg("");
     }
 
     const loginButton = (
@@ -33,18 +47,19 @@ function Signup(){
                     <h2>Signup</h2>
 
                     <label>Username</label>
-                    <input type="text" className={Border.inputBorder} />
+                    <input type="text" className={Border.inputBorder} onChange={(e) => {setUsername(e.target.value); resetErrorMsg()}} required />
 
                     <label>Password</label>
-                    <input type="password" className={Border.inputBorder} />
+                    <input type="password" className={Border.inputBorder} onChange={(e) => {setPassword(e.target.value); resetErrorMsg()}} required />
 
                     <label>Confirm Password</label>
-                    <input type="password" className={Border.inputBorder} />
+                    <input type="password" className={Border.inputBorder} onChange={(e) => {setPassword2(e.target.value); resetErrorMsg()}} required />
 
-                    <button type="submit" onClick={handleSignUp}>Signup</button>
+                    <button type="submit" className={Border.buttonBorder} onClick={handleSignUp}>Signup</button>
+                    <p className="signupError">{signupErrorMsg}</p>                
                 </form>
                 <div id="login">
-                    <p>Already have an account?&nbsp;</p>
+                    <p>Have an account?&nbsp;</p>
                     {loginButton}
                 </div>
             </div>

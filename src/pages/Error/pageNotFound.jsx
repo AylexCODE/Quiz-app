e03847e4a51b1368b0048a1b82333ee278245708
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-02-04 12:06:33 +0000

    Added Auto Redirect to Home Page

--- src/pages/Error/pageNotFound.jsx
+++ src/pages/Error/pageNotFound.jsx
@@ -1,6 +1,18 @@
+import './pageNotFound.css';
+
+import { useNavigate } from 'react-router-dom';
+
 function ErrorPage(){
+    const navigate = useNavigate();
+
+    setTimeout(() => {
+        navigate("/", { replace: true });
+    }, 5000);
+
     return (
-        <p>Error Page Not Found!</p>
+        <main className="pageNotFoundError">
+            <p><span className="errorText">404</span>Error Page Not Found!</p>
+        </main>
     );
 }
 

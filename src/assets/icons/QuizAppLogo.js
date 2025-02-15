404a520a56880c941970a977fd12311b725ca2c6
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-02-15 12:18:25 +0000

    Setting App Logo

--- src/assets/icons/QuizAppLogo.js
+++ src/assets/icons/QuizAppLogo.js
@@ -1,13 +1,25 @@
+import iconStyle from './iconStyle.module.css';
+
 function QuizAppLogo(){
     return (
+        <div className={iconStyle.appLogo}>
         <svg width="64px" height="64px" viewBox="0 0 64 64">
+<path layer="1" fill="#000000" fillOpacity="1.0" stroke="#000000" stroke-width="1.0" stroke-opacity="1.0" stroke-miterlimit="10" d="M24.26,63.43C24.26,63.43,55.04,50.97,55.04,50.97C55.04,50.97,62.18,27.99,62.18,27.99C62.18,27.99,42.69,-0.0,42.69,-0.0C42.69,-0.0,12.0,9.96,12.0,9.96C12.0,9.96,1.5,36.24,1.5,36.24C1.5,36.24,8.87,44.97,24.26,63.43Z" stroke-linecap="round"/>
+<path layer="1" fill="#FFFFFF" fillOpacity="1.0" stroke="#FFFFFF" stroke-width="1.0" stroke-opacity="1.0" stroke-miterlimit="10" d="M48.29,35.86C48.29,35.86,54.59,34.35,54.59,34.35C54.59,34.35,52.01,40.85,52.01,40.85C52.01,40.85,50.88,39.31,48.29,35.86Z" stroke-linecap="round"/>
+<path layer="1" fill="#FFFFFF" fillOpacity="1.0" stroke="#FFFFFF" stroke-width="3.0" stroke-opacity="1.0" stroke-miterlimit="10" d="M31.51,35.33C31.51,35.33,28.27,52.67,28.27,52.67C28.27,52.67,31.51,35.33,31.51,35.33Z" stroke-linecap="round"/>
+<path layer="1" fill="#FFFFFF" fillOpacity="1.0" stroke="#FFFFFF" stroke-width="3.0" stroke-opacity="1.0" stroke-miterlimit="10" d="M40.87,46.68C40.87,46.68,19.12,40.48,19.12,40.48C19.12,40.48,40.87,46.68,40.87,46.68Z" stroke-linecap="round"/>
+<path layer="1" fill="#FFFFFF" fillOpacity="0.0" stroke="#FFFFFF" stroke-width="2.0" stroke-opacity="1.0" stroke-miterlimit="10" d="M33.31,8.0C33.31,8.0,16.13,24.01,16.13,24.01C16.13,24.01,13.61,20.64,13.61,20.64" stroke-linecap="round"/>
+<path layer="1" fill="#FFFFFF" fillOpacity="1.0" stroke="#FFFFFF" stroke-width="1.0" stroke-opacity="1.0" stroke-miterlimit="10" d="M42.93,13.57C42.93,13.57,54.99,27.84,54.99,27.84C54.99,27.84,46.59,28.99,43.47,29.45C43.47,29.45,39.85,25.01,39.85,25.01C39.85,25.01,40.71,21.87,42.93,13.57Z" stroke-linecap="round"/>
+            {/*
             <path layer="1" fill="#000000" fillOpacity="1.0" stroke="#000000" strokeWidth="1.0" strokeOpacity="1.0" strokeMiterlimit="10" d="M27.74,51.21C27.74,51.21,45.33,43.86,45.33,43.86C45.33,43.86,49.41,30.3,49.41,30.3C49.41,30.3,38.27,13.78,38.27,13.78C38.27,13.78,20.73,19.66,20.73,19.66C20.73,19.66,14.73,35.17,14.73,35.17C14.73,35.17,18.94,40.32,27.74,51.21Z" strokeLinecap="round"/>
             <path layer="1" fill="#FFFFFF" fillOpacity="1.0" stroke="#FFFFFF" strokeWidth="1.0" strokeOpacity="1.0" strokeMiterlimit="10" d="M41.47,34.94C41.47,34.94,45.07,34.05,45.07,34.05C45.07,34.05,43.6,37.89,43.6,37.89C43.6,37.89,42.95,36.98,41.47,34.94Z" strokeLinecap="round"/>
             <path layer="1" fill="#FFFFFF" fillOpacity="1.0" stroke="#FFFFFF" strokeWidth="3.0" strokeOpacity="1.0" strokeMiterlimit="10" d="M31.88,34.63C31.88,34.63,30.03,44.86,30.03,44.86Z" strokeLinecap="round"/>
             <path layer="1" fill="#FFFFFF" fillOpacity="1.0" stroke="#FFFFFF" strokeWidth="3.0" strokeOpacity="1.0" strokeMiterlimit="10" d="M37.23,41.33C37.23,41.33,24.8,37.67,24.8,37.67Z" strokeLinecap="round"/>
             <path layer="1" fill="#FFFFFF" fillOpacity="0.0" stroke="#FFFFFF" strokeWidth="2.0" strokeOpacity="1.0" strokeMiterlimit="10" d="M32.91,18.5C32.91,18.5,23.09,27.95,23.09,27.95C23.09,27.95,21.65,25.96,21.65,25.96" strokeLinecap="round"/>
             <path layer="1" fill="#FFFFFF" fillOpacity="1.0" stroke="#FFFFFF" strokeWidth="1.0" strokeOpacity="1.0" strokeMiterlimit="10" d="M38.41,21.79C38.41,21.79,45.3,30.21,45.3,30.21C45.3,30.21,40.5,30.89,38.72,31.16C38.72,31.16,36.65,28.54,36.65,28.54C36.65,28.54,37.14,26.69,38.41,21.79Z" strokeLinecap="round"/>
+            */}
         </svg>
+        </div>
     )
 }
 

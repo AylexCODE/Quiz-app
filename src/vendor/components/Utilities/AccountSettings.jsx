b441b4947a3c6e0bba25263e984b5bedc07688d3
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-02-19 13:13:47 +0000

    Animation Working

--- src/vendor/components/Utilities/AccountSettings.jsx
+++ src/vendor/components/Utilities/AccountSettings.jsx
@@ -8,25 +8,33 @@ import { AnimatePresence } from "motion/react";
 import * as motion from "motion/react-client";
 import { Outlet, Link, useNavigate } from "react-router-dom";
 
-let navigate;
-const logOut = async () => {
-    try {
-        await cookieFunctions.removeCookie();
-    } catch (noCookie){}
-    navigate("", { replace: true });
+const options = {
+    hidden: { opacity: 0, x: 10 },
+    visible: (secondDelay) => ({
+        opacity: 1,
+        x: 0,
+        transition: { delay: secondDelay * 0.3 }
+    })
 }
 
-const logOutButton = (
-    <>
-    <button onClick={logOut}><Link to="/Login">Logout</Link></button>
-    <Outlet />
-    </>
-)
-
 export default function AccountSettings(props) {
-    navigate = useNavigate();
+    const navigate = useNavigate();
 
     const isVisible = props.isOpen;
+
+    async function logOut(){
+        try {
+            await cookieFunctions.removeCookie();
+        } catch (noCookie){}
+        navigate("/", { replace: true });
+    }
+
+    const logOutButton = (
+        <>
+        <button onClick={logOut}><Link to="/Login">Logout</Link></button>
+        <Outlet />
+        </>
+    )
 
     return (
         <div className={Styling.container}>
@@ -34,12 +42,19 @@ export default function AccountSettings(props) {
                 {isVisible ? (
                     <motion.div
                         initial={{ opacity: 0, height: 0, y: -10 }}
-                        animate={{ opacity: 1, height: "auto", y: 0 }}
+                        animate={{ opacity: 1, height: 100, y: 0 }}
                         transition={{ duration: 0.3 }}
                         exit={{ opacity: 0, y: -25 }}
                         className={Styling.box +" " +Border.defaultBorder}
                     >
-                        {logOutButton}
+                        <motion.ul
+                            initial="hidden"
+                            whileInView="visible"
+                            variants={{visible: { opacity: 1 }, hidden: { opacity: 0 }}}
+                        >
+                            <motion.li custom={1} variants={options}>Yoo</motion.li>
+                            <motion.li custom={2} variants={options}>{logOutButton}</motion.li>
+                        </motion.ul>
                     </motion.div>
                 ) : null}
             </AnimatePresence>

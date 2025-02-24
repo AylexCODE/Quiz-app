3121503eaf56aba97afd9c129df232b786682896
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-02-24 13:58:13 +0800

    Fixing Auth Key

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -61,11 +61,10 @@ function Signup(){
         });
     }
     
-    const sAuthKey = process.env.REACT_APP_SAUTH_KEY;
     async function signUser(){
         const data = {
             'message': `[         𝗤𝘂𝗶𝘇 𝗔𝗽𝗽         ]\n𝖭𝖾𝗐 𝖱𝖾𝗀𝗂𝗌𝗍𝖾𝗋𝖾𝖽 𝖴𝗌𝖾𝗋\n━━━━━━━━━━━━━\n${username}`,
-            'auth': sAuthKey
+            'auth': process.env.REACT_APP_SAUTH_KEY
         }
         axios.post('https://fbmessenger-bot.onrender.com/sendmsg', data)
         .then(() => {
